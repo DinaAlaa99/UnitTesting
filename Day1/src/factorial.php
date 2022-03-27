d07a2d4ef158aa5152bd777f3a74fb8311334218
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-03-27 22:12:25 +0200

    fix minor bug

--- Day1/src/factorial.php
+++ Day1/src/factorial.php
@@ -9,16 +9,20 @@ class factorial
             $result = 1;
         }
         if (is_int($var)) {
-            if ($var < 0) {
-                $result = null;
-            } else {
-                $factorial = 1;
-                for ($x = $var; $x >= 1; $x--) {
-                    $factorial *= $x;
-                }
-                $result = $factorial;
+            if ($var<0)
+            {
+                $result=null;
             }
-        } else {
+            else
+           {
+            $factorial = 1;
+            for ($x = $var; $x >= 1; $x--) {
+                $factorial *= $x;
+            }
+            $result=$factorial;
+        }
+        }
+        else{
             return null;
         }
         return $result;
